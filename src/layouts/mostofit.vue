3f1b02ca98c19b415629764e54540389f024b8d4
Aurora Selimi <selimi.aurora@gmail.com>
2018-03-17 18:32:27 +0100

    adding one

--- src/layouts/mostofit.vue
+++ src/layouts/mostofit.vue
@@ -1,142 +1,199 @@
-<style lang="scss" scoped>
-
+<style lang="scss">
 .topBari1 {
   position: fixed;
   top: 0;
   left: 0;
+  z-index: 3;
   display: flex;
-  flex-direction:row;
+  flex-direction: row;
   width: 100%;
-  height: 5%;
+  height: 50px;
+  border-bottom: solid 1px rgba(gray, .2);
   padding: 5px;
   background-color: white;
-  align-items:center;
-  justify-content:space-around;
-  .fedher{
+  align-items: center;
+  justify-content: space-between;
+  .fedher {
     display: flex;
-    align-items:center;
-    stroke:gray;
+    align-items: center;
+    stroke: gray;
   }
-  svg{
+  svg {
     display: flex;
-    stroke:rgba(gray, .7);
-    width: 20px;
+    stroke: rgba(gray, .7);
+    width: 40px;
     height: 20px;
-    margin:0 10px;
+    margin: 0 10px;
   }
-  .menu{
-    display: flex;  
-    align-items:center;
-  }
-  .leftside{
+  .menu {
     display: flex;
-    align-items:center;
+    align-items: center;
+  }
+  .leftside {
+    display: flex;
+    align-items: center;
     font-weight: 500;
-    font-size:22px;
+    font-size: 22px;
     letter-spacing: -2px;
-    p{
+    p {
       width: 100%;
       height: 100%;
     }
-    svg{
-      stroke:white;
-      fill:white;
-      width: 15px;
-      height: 15px;
+    svg {
+      stroke: white;
+      fill: white;
     }
-    :first-child{
-      border:solid 1px red;
+     :first-child {
+      border: solid 1px red;
       border-radius: 7px;
-      padding:2px 10px;
-      margin-right:4px;
+      padding: 2px 10px;
+      margin-right: 4px;
       background-color: red;
-
     }
   }
-  .input{
+  .input {
     display: flex;
-    .search{
+    .search {
       border: solid 1px rgba(gray, .5);
-      padding: 3px 20px;
+      padding: 5px 20px;
       display: flex;
-      align-items:center;
+      align-items: center;
       background-color: whitesmoke;
-      svg{
-        stroke:rgba(gray, .6);
+      svg {
+        stroke: rgba(gray, .6);
         width: 16px;
-        height:16px;
+        height: 16px;
       }
     }
-    input{
+    input {
       width: 500px;
-      padding: 3px 20px;
+      padding: 5px 20px;
       font-size: 16px;
-    }      
-  }
-}  
-.topBari2{
-  position: fixed;
-  top:6%;
-  border:solid 1px rgba(gray, .2);
-  height:max-content;
-  background-color: white;
-  width: 100%;
-  padding: 10px;
-  display: flex;
-  flex-direction:row;
-  align-items:center;
-  justify-content:space-between;
-  .hts{
-    width: 25%;
-    margin-left:auto;
-    margin-right:auto;
-    display: flex;
-    align-items:center;
-    justify-content:space-between;
-    :first-child{
-      border-bottom: solid 1px red;
+      border: solid 1px rgba(gray, .5);
+      border-radius: 0;
     }
   }
 }
 
+.home {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  margin-top: 50px;
+  .topBari2 {
+    position: fixed;
+    right: 0;
+    border-bottom: solid 1px rgba(gray, .2);
+    border-left: solid 1px rgba(gray, .2);
+    height: max-content;
+    background-color: white;
+    width: 100%;
+    padding: 10px 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    .hts {
+      width: 25%;
+      margin: auto;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      :first-child {
+        border-bottom: solid 1px red;
+      }
+    }
+  }
+  .anash {
+    position: sticky;
+    top:50px;
+    display: flex;
+    flex: 1;
+    z-index: 2;
+    max-width: 200px;
+    flex-direction: column;
+    background-color: white;
+    height: 100vh;
+    padding: 5px 20px;
+    border-right: solid 1px rgba(gray, .2);
+    :first-child {
+      border: none;
+    }
+    ul {
+      list-style-type: none;
+      margin:0;
+      padding: 10px;
+      border-top: solid 1px rgba(gray, .3);
+      :first-child{
+        color:red;
+      }
+      li{
+        padding: 5px;
+      }
+    }
+  }
+}
 
 </style>
-
 <template>
   <div>
-    <div id="topBari">
-      <div class="topBari1">
-        <div class="menu">
-          <span ><feather-icon type="menu"></feather-icon></span>
-          <span class="leftside" ><feather-icon type="play"></feather-icon><p>YouTube</p></span>
+    <div class="topBari1">
+      <div class="menu">
+        <span @click="shfaqMenu()" ><feather-icon type="menu"></feather-icon></span>
+        <span class="leftside"><feather-icon type="play"></feather-icon><p>YouTube</p></span>
+      </div>
+      <div class="input">
+        <input type="text" placeholder="Search">
+        <div class="search">
+          <feather-icon type="search"></feather-icon>
         </div>
-        <div class="input">
-          <input type="text" placeholder="Search">
-          <div  class="search"><feather-icon type="search"></feather-icon></div>
-        </div>
-        <div class="fedher" > 
-          <span><feather-icon type="upload"></feather-icon></span>
-          <span><feather-icon type="bell"></feather-icon></span>
-          <span><feather-icon type="image"></feather-icon></span>
-        </div>
-      </div>  
-      
+      </div>
+      <div class="fedher">
+        <span><feather-icon type="upload"></feather-icon></span>
+        <span><feather-icon type="bell"></feather-icon></span>
+        <span><feather-icon type="image"></feather-icon></span>
+      </div>
+    </div>
+    <div class="home">
       <div class="topBari2">
-        <div>
-        </div>
         <div class="hts">
           <span>Home</span>
           <span>Trending</span>
           <span>Subscriptions</span>
         </div>
       </div>
+      <div class="anash" v-if="menuja">
+        <ul>
+          <li>Home</li>
+          <li>My Channel</li>
+          <li>Trending</li>
+          <li>Subscriptions</li>
+          <li>Youtube Red</li>
+        </ul>
+        <ul>
+          <li>Library</li>
+          <li>History</li>
+          <li>Watch Later</li>
+          <li>Liked Videos</li>
+        </ul>
+        <ul>
+          <li>Subscriptions</li>
+        </ul>
+      </div>
+      <slot></slot>
     </div>
-    <slot></slot> 
   </div>
 </template>
 <script>
+export default {
+  data: () => ({
+    menuja:true,
+  }),
+  methods:{
+    shfaqMenu(){
+      this.menuja=!this.menuja;
 
-
-  export default {
-  };
+    }
+  },
+};
 </script>
